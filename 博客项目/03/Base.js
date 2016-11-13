215517dc08d06fb12b221ffa9c16c570f48878e6
goWithHappy <2440138281@qq.com>
2016-11-13 16:54:09 +0800

    finish 04

--- 博客项目/03/Base.js
+++ 博客项目/03/Base.js
@@ -1,25 +1,30 @@
-function $(){
+
+
+//前台调用,在使用该封装库时，每次创建的都是新的对象
+var $ = function () {
 	return new Base();
 }
 
-//封装成后缀式
-function Base(){
-	//创建一个数组用来封装节点和节点数组
-	this.elements=[]; //使用this创建相当于非静态变量，通过var创建相当于静态全局变量
-	// 获取ID节点
-	this.getId=function(id){
-		this.elements.push(document.getElementById(id));
-		return this;
+//基础库,Base是构造函数其他属性是根据其来进行扩充的
+function Base() {}
+
+//创建一个数组，来保存获取的节点和节点数组,是一个全局变量用来保存产生的值
+Base.prototype.elements = [];
+
+//获取ID节点
+Base.prototype.getId = function (id) {
+	this.elements.push(document.getElementById(id));
+	return this;
+};
+
+//获取元素节点数组
+Base.prototype.getTagName = function (tag) {
+	var tags = document.getElementsByTagName(tag);
+	for (var i = 0; i < tags.length; i ++) {
+		this.elements.push(tags[i]);
 	}
-	//通过Tag获取节点
-	this.getTagName=function(tag){
-		var tags=document.getElementsByTagName(tag);
-		for (var i = 0; i < tags.length; i++) {
-			this.elements.push(tags[i]);
-		}
-		return this;
-	}
-}
+	return this;
+};
 
 //获取CLASS节点数组
 Base.prototype.getClass = function (className, idName) {
@@ -38,33 +43,86 @@ Base.prototype.getClass = function (className, idName) {
 	return this;
 }
 
-//获取某一个节点
-Base.prototype.getElement=function(num){
-	var element=this.elements[num];
-	this.elements=[];
-	this.elements[0]=element;
-	return this;
-}
 
-//封装一些连缀操作的方法
-Base.prototype.css=function(attr,value){
-	for (var i = 0; i < this.elements.length; i++) {
-		//如果传入一个参数，返回attr对应的属性值
-		if (arguments.length==1) {
-			if (typeof window.getComputedStyle!='undefined') {	//W3C
-				return window.getComputedStyle(this.elements[i],null)[attr];
-			}else if (typeof this.elements[i].CurrentStyle!='undefined') {	//IE
-				return this.elements[i].CurrentStyle[attr];
-			}	
+//获取某一个节点
+Base.prototype.getElement = function (num) {	
+	var element = this.elements[num];
+	this.elements = [];
+	this.elements[0] = element;
+	return this;
+};
+
+//设置CSS
+Base.prototype.css = function (attr, value) {
+	for (var i = 0; i < this.elements.length; i ++) {
+		if (arguments.length == 1) {
+			if (typeof window.getComputedStyle != 'undefined') {//W3C
+				return window.getComputedStyle(this.elements[i], null)[attr];
+			} else if (typeof this.elements[i].currentStyle != 'undeinfed') {//IE
+				return this.elements[i].currentStyle[attr];
+			}
 		}
-		this.elements[i].style[attr]=value;
+		this.elements[i].style[attr] = value;
 	}
 	return this;
 }
 
+//添加class
+Base.prototype.addClass=function(className)
+{
+	for(var i=0;i<this.elements.length;i++)
+	{
+		//如果原来的elements数组中的某个属性没有改class，则为该属性添加该class
+		if(!this.elements[i].className.match(new RegExp('(\\s|^)' + className + '(\\s|$)')))
+		{
+			this.elements[i].className+=''+className;
+		}
+	}
+	return this;
+}
+
+//移除Class,操作Class
+Base.prototype.removeClass=function(className){
+	for(var i=0;i<elements.length;i++)
+	{
+		if(this.elements[i].className.match(new RegExp('(\\s|^)' + className + '(\\s|$)')))
+		{
+			//将原来的class用空字符替代
+			this.elements[i].className = this.elements[i].className.
+				replace(new RegExp('(\\s|^)' + className + '(\\s|$)'), ' ');
+		}
+	}
+	return this;
+}
+
+//设置link或者style中的CSS规则
+Base.prototype.addRule=function(num,selectorText,cssText,position)
+{
+	var sheet = document.styleSheets[num];
+	if (typeof sheet.insertRule != 'undefined') {
+		sheet.insertRule(selectorText + "{" + cssText + "}", position);
+	} else if (typeof sheet.addRule != 'undefined') {
+		sheet.addRule(selectorText, cssText, position);
+	}
+}
+
+//移除link和CSS规则
+Base.prototype.removeRule=fucntion(num,index)
+{
+	var sheet=document.styleSheets[num];
+	if(typeof sheet.deleteRule!='undefined')
+	{
+		sheet.deleteRule(index);
+	}
+	else if(typeof sheet.removeRule)
+	{
+		sheet.removeRule(index);
+	}
+}
+//设置innerHTML
 Base.prototype.html = function (str) {
 	for (var i = 0; i < this.elements.length; i ++) {
-		if (arguments.length==1) {
+		if (arguments.length == 0) {
 			return this.elements[i].innerHTML;
 		}
 		this.elements[i].innerHTML = str;
@@ -72,9 +130,28 @@ Base.prototype.html = function (str) {
 	return this;
 }
 
+//触发点击事件,为每一个元素都绑定事件
 Base.prototype.click = function (fn) {
 	for (var i = 0; i < this.elements.length; i ++) {
 		this.elements[i].onclick = fn;
 	}
 	return this;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 博客项目/05/index.js
@@ -0,0 +1,15 @@
+window.onload=function()
+{
+	//第一个参数是鼠标移入时所触发的函数，第二个参数时鼠标移除时所触发的函数,
+	//由于该函数是在某个HTML对象内部所触发，说以当前的this指代的是该HTML对象
+	$().getClass('memeber').hover(function(){
+		// 设置鼠标放上去时箭头的方向
+		$(this).css('background','url(images/arrow2.png) no-repeat 65px center');
+		//设置函数的显示与隐藏
+		$().getTagName('ul').show();
+	},function(){
+		//鼠标离开时恢复原状
+		$(this).css('background','url(images/arrow.png) no-repeat 65px center');
+		$().getTagName('ul').hide();
+	});
+}
